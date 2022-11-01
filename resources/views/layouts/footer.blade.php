89778effe05e4a0f3997c80ff1e0cf97b828e264
Emanuel Saucedo <emanuelsaucedo52@gmail.com>
2022-11-01 01:33:09 -0600

    mockup alta fidelidad landingpage

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,16 +1,5 @@
 <footer class="footer mt-4">
-    <div class="bg-light py-4">
-        <div class="container">   
-            <div class="row">
-                <div class="col-lg-6">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente veritatis iusto rem? Aliquam, quo vero. Dolorem, officiis doloremque. Corrupti, ipsa voluptates! Consequuntur suscipit necessitatibus harum aliquid nulla debitis molestias atque.</p>
-                </div>
-                <div class="col-lg-6">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente veritatis iusto rem? Aliquam, quo vero. Dolorem, officiis doloremque. Corrupti, ipsa voluptates! Consequuntur suscipit necessitatibus harum aliquid nulla debitis molestias atque.</p>
-                </div>
-            </div>
-        </div>
-    </div>
+    
     <div class="bg-dark py-2">
         <div class="container text-light text-center ">
             <div class="row">
--- resources/views/layouts/master-landing.blade.php
+++ resources/views/layouts/master-landing.blade.php
@@ -4,14 +4,69 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>SnapUp</title>
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
     
+
+    <div>
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+             <div class="d-flex">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <form class="d-flex" role="search">
+                  <input class="form-control p-2" type="search" placeholder="Search" aria-label="Search">
+                </form>
+             </div>
+             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                  <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                          </a>
+                          <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                          </ul>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link disabled">Disabled</a>
+                        </li>
+                      </ul>
+                      <div>
+                          <button class="btn btn-outline-success" type="submit">Search</button>
+                          <button class="btn btn-outline-success" type="submit">Search</button>
+                      </div>
+                  
+                </div>
+              </div>
+              
+              
+              
+            </div>
+          </nav>
+    </div>
+
+
     @yield('content')
-    <div id="roles"></div>
     @include('layouts.footer')
 
     <script src="{{asset('js/app.js')}}"></script>
