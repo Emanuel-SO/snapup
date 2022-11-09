80e212eedad3d04571daba66d9803918c66092e7
LouckGuntz <emanuelsaucedo52@gmail.com>
2022-11-08 18:29:17 -0600

    0.1.11 - correcion carrito

--- resources/views/carrito.blade.php
+++ resources/views/carrito.blade.php
@@ -26,20 +26,7 @@
                         </div>
                     </div>
 
-                    <div class="card mb-3" style="max-width: 540px;">
-                        <div class="row g-0">
-                            <div class="col-md-4">
-                                <img src="https://placehold.jp/350x350.png" class="img-fluid rounded-start" alt="...">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    
                     <h2 class="h3 p-5 text-end">Subtotal (n producto):$</h2>
                 </div>
             </div>
